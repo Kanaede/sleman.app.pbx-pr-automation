67855f4e2fda9bcf7e95c9e2a64542127dbcfb23
Kanade <hirotakanarein@gmail.com>
2026-07-18 19:27:23 +0700

    update v1.1 refactor main.py to encapsulate functionality within AutoPresence class and improve menu handling

--- main.py
+++ main.py
@@ -10,39 +10,12 @@ from pydoll.browser.chromium import Chrome
 
 load_dotenv()
 
-SERVER = os.getenv("LOGIN_SERVER")
-wt = 1
-running = True
-
-def clr_screen():
-    subprocess.run('cls' if os.name == 'nt' else 'clear')
-
-async def main_menu():
-
-    menu_actions = {
-        '1':{"title": "Absent all", "func": all_account},
-        '2':{"title": "Just an account", "func": one_account},
-        '3':{"title": "Custom account", "func": custom_account},
-        '0':{"title": "Exit", "func": program_exit},
-    }
-
-    while running:
-        clr_screen()
-        menu_header("Available Command:")
-        for key, value in menu_actions.items():
-            print(f"[{key}] {value['title']}")
-
-        choice = input("\nSelect an option: ").strip()
-
-        if choice in menu_actions:
-            await menu_actions[choice]["func"]()
-        else:
-            print("Invalid selection. Please try again.")
-            await asyncio.sleep(wt)
-
 def get_accounts():
     with open("data/accounts.yaml", "r") as file:
         return yaml.safe_load(file)
+
+def clr_screen():
+    subprocess.run('cls' if os.name == 'nt' else 'clear')
 
 def menu_header(text=None):
     clr_screen()
@@ -51,136 +24,167 @@ def menu_header(text=None):
     if text is not None:
         print(text)
 
-async def one_account():
-    accounts = get_accounts()
-    if not accounts:
-        print("You don't have any saved account.")
-        await asyncio.sleep(2)
-        return False
-    
-    n = 1
-    account_list = {}
-    for key, value in accounts.items():
-        account_list[f"{n}"] = {"email": key, "password": value}
-        n += 1
-    
-    l = True
-    while l:
-        menu_header("One account to record.")
-        for key, value in account_list.items():
-            print(f"  [{key}] {value['email']}")
+class AutoPresence():
+    def __init__(self):
+        self.SERVER = os.getenv("LOGIN_SERVER")
+        self.wt = 1
+        self.running = True
 
-        print("\nType 0 to cancel")
-        select = input("Select an account (number): ")
+    async def main_menu(self):
 
-        if select.strip() == "0":
-            l = False
+        menu_actions = {
+            '1':{"title": "Absent all", "func": self.all_account},
+            '2':{"title": "Just an account", "func": self.one_account},
+            '3':{"title": "Custom account", "func": self.custom_account},
+            '0':{"title": "Exit", "func": self.program_exit},
+        }
+
+        while self.running:
+            clr_screen()
+            menu_header("Available Command:")
+            for key, value in menu_actions.items():
+                print(f"[{key}] {value['title']}")
+
+            choice = input("\nSelect an option: ").strip()
+
+            if choice in menu_actions:
+                await menu_actions[choice]["func"]()
+            else:
+                print("Invalid selection. Please try again.")
+                await asyncio.sleep(self.wt)
+
+    async def one_account(self):
+        accounts = get_accounts()
+        if not accounts:
+            print("You don't have any saved account.")
+            await asyncio.sleep(2)
             return False
 
-        if select in account_list:
-            l = False
-            grab_account = account_list[select]
-            email = grab_account["email"]
-            password = grab_account["password"]
-            await accounting({email:password})
+        n = 1
+        account_list = {}
+        for key, value in accounts.items():
+            account_list[f"{n}"] = {"email": key, "password": value}
+            n += 1
+
+        l = True
+        while l:
+            menu_header("One account to record.")
+            for key, value in account_list.items():
+                print(f"  [{key}] {value['email']}")
+
+            print("\nType 0 to cancel")
+            select = input("Select an account (number): ")
+
+            if select.strip() == "0":
+                l = False
+                return False
+
+            if select in account_list:
+                l = False
+                grab_account = account_list[select]
+                email = grab_account["email"]
+                password = grab_account["password"]
+                await self.accounting({email:password})
+            else:
+                print("\nInvalid selecton. Please try again.")
+                await asyncio.sleep(self.wt)
+
+    async def all_account(self):
+        accounts = get_accounts()
+        if accounts:
+            await self.accounting(accounts)
         else:
-            print("\nInvalid selecton. Please try again.")
-            await asyncio.sleep(wt)
-
-async def all_account():
-    accounts = get_accounts()
-    if accounts:
-        await accounting(accounts)
-    else:
-        print("You don't have any saved account.")
-        await asyncio.sleep(2)
-        return False
-
-async def custom_account():
-    menu_header("Absence for custom account\n")
-
-    account = {
-        "email":"your.email@gmail.com",
-        "password":"1234568"
-    }
-
-    print("Type 0 to cancel")
-    email = input("Enter an email: ").strip()
-    if email.strip() == "0":
-        return False
-    elif email == "":
-        print("Email or password can't be null")
-        await asyncio.sleep(wt)
-        return False
-
-    password = input("Enter the password: ").strip()
-
-    if email or password is None:
-        print("Email or password can't be null")
-        await asyncio.sleep(wt)
-        return False
-
-    account.update({"email":email})
-    account.update({"password":password})
-
-    await accounting(account)
-
-async def accounting(accounts:dict):
-    for email, password in accounts.items():
-        await simulation(email, password)
-
-async def simulation(email, password):
-    async with Chrome() as browser:
-        tab = await browser.start()
-        await browser.set_window_maximized()
-        tab.mouse.debug = True
-        async with tab.expect_and_bypass_cloudflare_captcha():
-            await tab.go_to(SERVER)
-
-        print('Turnstile handled, continuing...')
-
-        email_field = await tab.find(tag_name="input", type="email", name="email")
-        await email_field.type_text(text=email)
-        
-        password_field = await tab.find(tag_name="input", type="password", name="password")
-        await password_field.type_text(text=password, humanize=True)
-
-        login_submit = await tab.find(tag_name="button", type="submit")
-        await login_submit.click()
-
-        error_element = await tab.find(
-            css_selector=".text-danger",
-            text="These credentials do not match our records.",
-            timeout=4,
-            raise_exc=False,
-        )
-        
-        if error_element:
-            print(f"Login Failed for {email}: Invalid credentials.")
+            print("You don't have any saved account.")
+            await asyncio.sleep(2)
             return False
-        
-        try:
-            presence_button = await tab.find(tag_name="button", type="button", data_bs_target="#modalPresensi")
-            await presence_button.click(humanize=True)
 
-            await asyncio.sleep(1)
+    async def custom_account(self):
+        menu_header("Absence for custom account\n")
 
-            presence = await tab.query("//input[@value='1']/..")
-            await presence.click(humanize=True)
+        account = {
+            "email":"your.email@gmail.com",
+            "password":"1234568"
+        }
 
-            presence_submit = await tab.find(xpath="//button[contains(., 'Simpan Presensi')]")
-            await presence_submit.click(humanize=True)
-        
-        except:
-            pass
+        print("Type 0 to cancel")
+        email = input("Enter an email: ").strip()
+        if email.strip() == "0":
+            return False
+        elif email == "":
+            print("Email or password can't be null")
+            await asyncio.sleep(self.wt)
+            return False
 
-        print("Presence has been checked.")
-        traceback.print_exc
-        return
+        password = input("Enter the password: ").strip()
 
-async def program_exit():
-    print("Exiting program...")
-    running = False
-    sys.exit(0)
+        if email or password is None:
+            print("Email or password can't be null")
+            await asyncio.sleep(self.wt)
+            return False
 
-asyncio.run(main_menu())
+        account.update({"email":email})
+        account.update({"password":password})
+
+        await self.accounting(account)
+
+    async def accounting(self, accounts:dict):
+        for email, password in accounts.items():
+            await self.simulation(email, password)
+
+    async def simulation(self, email, password):
+        async with Chrome() as browser:
+            tab = await browser.start()
+            await browser.set_window_maximized()
+            tab.mouse.debug = True
+            async with tab.expect_and_bypass_cloudflare_captcha():
+                await tab.go_to(self.SERVER)
+
+            print('Turnstile handled, continuing...')
+
+            email_field = await tab.find(tag_name="input", type="email", name="email")
+            await email_field.type_text(text=email)
+
+            password_field = await tab.find(tag_name="input", type="password", name="password")
+            await password_field.type_text(text=password, humanize=True)
+
+            login_submit = await tab.find(tag_name="button", type="submit")
+            await login_submit.click()
+
+            error_element = await tab.find(
+                css_selector=".text-danger",
+                text="These credentials do not match our records.",
+                timeout=4,
+                raise_exc=False,
+            )
+
+            if error_element:
+                print(f"Login Failed for {email}: Invalid credentials.")
+                return False
+
+            try:
+                presence_button = await tab.find(tag_name="button", type="button", data_bs_target="#modalPresensi")
+                await presence_button.click(humanize=True)
+
+                await asyncio.sleep(1)
+
+                presence = await tab.query("//input[@value='1']/..")
+                await presence.click(humanize=True)
+
+                presence_submit = await tab.find(xpath="//button[contains(., 'Simpan Presensi')]")
+                await presence_submit.click(humanize=True)
+
+            except:
+                pass
+
+            print("Presence has been checked.")
+            traceback.print_exc
+            return
+
+    async def program_exit(self):
+        print("Exiting program...")
+        self.running = False
+        sys.exit(0)
+
+autobot = AutoPresence()
+
+asyncio.run(autobot.main_menu())
